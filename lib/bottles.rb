02c73c363a0d883437e2629a906f57deeca951c3
Katsuragawa <andre.katsuragawa@gmail.com>
2021-11-27 08:42:19 -0300

    add 1.1 example

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,65 +1,19 @@
 class Bottles
-  def verse(count)
-    first_phrase(count) +
-      second_phrase(count) +
-      third_phrase(count) +
-      last_phrase(count - 1)
-  end
-
-  def verses(start_count, end_count)
-    current = start_count
-    final_phrase = verse(current)
-    while current > end_count
-      current -= 1
-      final_phrase += "\n"
-      final_phrase += verse(current)
-    end
-    final_phrase
-  end
-
   def song
-    verses(99, 0)
+  verses(99, 0)
   end
 
-  private
-
-  def bottle_or_bottles?(count)
-    count > 1 ? "bottles" : "bottle"
+  def verses(hi, lo)
+  hi.downto(lo).map {|n| verse(n) }.join("\n")
   end
 
-  def first_phrase(count)
-    if count.positive?
-      "#{count} #{bottle_or_bottles?(count)} of beer on the wall, "
-    else
-      "No more bottles of beer on the wall, "
-    end
-  end
-
-  def second_phrase(count)
-    if count.positive?
-      "#{count} #{bottle_or_bottles?(count)} of beer.\n"
-    else
-      "no more bottles of beer.\n"
-    end
-  end
-
-  def third_phrase(count)
-    if count > 1
-      "Take one down and pass it around, "
-    elsif count.zero?
-      "Go to the store and buy some more, "
-    else
-      "Take it down and pass it around, "
-    end
-  end
-
-  def last_phrase(count)
-    if count.positive?
-      "#{count} #{bottle_or_bottles?(count)} of beer on the wall.\n"
-    elsif count.zero?
-      "no more bottles of beer on the wall.\n"
-    else
-      "99 bottles of beer on the wall.\n"
-    end
+  def verse(n)
+  "#{n == 0 ? 'No more' : n} bottle#{'s' if n != 1}" +
+  " of beer on the wall, " +
+  "#{n == 0 ? 'no more' : n} bottle#{'s' if n != 1} of beer.\n" +
+  "#{n > 0 ? "Take #{n > 1 ? 'one' : 'it'} down and pass it around"
+  : "Go to the store and buy some more"}, " +
+  "#{n-1 < 0 ? 99 : n-1 == 0 ? 'no more' : n-1} bottle#{'s' if n-1 != 1}"+
+  " of beer on the wall.\n"
   end
 end
