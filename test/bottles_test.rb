539f7f7270f74aac53150bd769a31cd08f76abb5
Katsuragawa <andre.katsuragawa@gmail.com>
2021-12-24 06:30:31 -0300

    change back to original tests

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -1,7 +1,7 @@
-gem "minitest", "~> 5.4"
-require "minitest/autorun"
-require "minitest/pride"
-require_relative "../lib/bottles"
+gem 'minitest', '~> 5.4'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/bottles'
 
 class BottlesTest < Minitest::Test
   def test_the_first_verse
@@ -22,24 +22,6 @@ class BottlesTest < Minitest::Test
     assert_equal expected, Bottles.new.verse(3)
   end
 
-  def test_verse_7
-    expected =
-      "7 bottles of beer on the wall, " +
-      "7 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 six-pack of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(7)
-  end
-
-  def test_verse_6
-    expected =
-      "1 six-pack of beer on the wall, " +
-      "1 six-pack of beer.\n" +
-      "Take one down and pass it around, " +
-      "5 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(6)
-  end
-
   def test_verse_2
     expected =
       "2 bottles of beer on the wall, " +
@@ -55,7 +37,7 @@ class BottlesTest < Minitest::Test
       "1 bottle of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(1)
+     assert_equal expected, Bottles.new.verse(1)
   end
 
   def test_verse_0
@@ -379,9 +361,9 @@ class BottlesTest < Minitest::Test
       Take one down and pass it around, 7 bottles of beer on the wall.
 
       7 bottles of beer on the wall, 7 bottles of beer.
-      Take one down and pass it around, 1 six-pack of beer on the wall.
+      Take one down and pass it around, 6 bottles of beer on the wall.
 
-      1 six-pack of beer on the wall, 1 six-pack of beer.
+      6 bottles of beer on the wall, 6 bottles of beer.
       Take one down and pass it around, 5 bottles of beer on the wall.
 
       5 bottles of beer on the wall, 5 bottles of beer.
