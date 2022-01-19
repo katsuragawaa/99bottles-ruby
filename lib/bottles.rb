3067577fd934ba640f784830bf3aafdd113c3ae3
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-18 21:17:24 -0300

    add polymorphism

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,18 +8,28 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = BottleNumber.new(number)
-    next_bottle_number = BottleNumber.new(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
 
     "#{bottle_number} of beer on the wall, ".capitalize +
       "#{bottle_number} of beer.\n" +
       "#{bottle_number.action}, " +
-      "#{next_bottle_number} of beer on the wall.\n"
+      "#{bottle_number.successor} of beer on the wall.\n"
   end
 end
 
 class BottleNumber
   attr_reader :number
+
+  def self.for(number)
+    case number
+    when 0
+      BottleNumber0
+    when 1
+      BottleNumber1
+    else
+      BottleNumber
+    end.new(number)
+  end
 
   def initialize(number)
     @number = number
@@ -30,22 +40,46 @@ class BottleNumber
   end
 
   def container
-    number == 1 ? 'bottle' : 'bottles'
+    'bottles'
   end
 
   def quantity
-    number == 0 ? 'no more' : number.to_s
+    number.to_s
   end
 
   def pronoun
-    number == 1 ? 'it' : 'one'
+    'one'
   end
 
   def action
-    number == 0 ? 'Go to the store and buy some more' : "Take #{pronoun} down and pass it around"
+    "Take #{pronoun} down and pass it around"
   end
 
   def successor
-    number == 0 ? 99 : number - 1
+    BottleNumber.for(number - 1)
+  end
+end
+
+class BottleNumber0 < BottleNumber
+  def quantity
+    'no more'
+  end
+
+  def action
+    'Go to the store and buy some more'
+  end
+
+  def successor
+    BottleNumber.for(99)
+  end
+end
+
+class BottleNumber1 < BottleNumber
+  def container
+    'bottle'
+  end
+
+  def pronoun
+    'it'
   end
 end
